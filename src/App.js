80542ead189353249610575db28107feace1e965
LeandroMagonza <leandro.magonza@gmail.com>
2022-12-04 13:04:09 -0300

    update

--- src/App.js
+++ src/App.js
@@ -8,40 +8,78 @@ import LogIn from './LogIn';
 import Personas from './Personas';
 const logo = require('./buildingLogo.png');
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const httpClient = () => {
+  const request = (method) => (url, data) => fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+   body: data ? JSON.stringify(data) : undefined 
+  })
+
+  return {
+    get: request("GET"),
+    post: request("POST"),
+    put: request("PUT"),
+    patch: request("PATCH"),
+    delete: request("DELETE")
+  }
+}
+
+const Layout = ({children}) => {
+return <div className="App">
+
+
+<nav class="navbar navbar-dark bg-dark">
+  <img src={logo} width="60" height="60" alt=""></img>
+
+  <span class="navbar-brand mb-1 h1">Consorciapp</span>
+  <div>
+
+    
+    <Button onClick={Logout}>Logout</Button>
+  </div>
+</nav>
+
+<br></br>
+{
+  children
+}
+</div>
+}
+
 function App() {
   const [usuarioLogueado, setUsuarioLogueado] = useState(null);
+  
   function Logout() {
     setUsuarioLogueado(null);
   }
-  let contenido;
-  if (usuarioLogueado != null) {
-    contenido = <Personas usuarioLogueado={usuarioLogueado}></Personas>;
+  
+  if(usuarioLogueado === null){
+    return <Layout>
+<LogIn setUsuarioLogueado={setUsuarioLogueado}></LogIn>
+    </Layout>
   }
-  else {
-    contenido = <LogIn setUsuarioLogueado={setUsuarioLogueado}></LogIn>;
-  }
 
-  return (
-    <div className="App">
+  return <Layout><Personas usuarioLogueado={usuarioLogueado}></Personas></Layout>;
+  
+  
+}
 
+const queryClient = new QueryClient()
 
-      <nav class="navbar navbar-dark bg-dark">
-        <img src={logo} width="60" height="60" alt=""></img>
-
-        <span class="navbar-brand mb-1 h1">Consorciapp</span>
-        <div>
-
-          <span class="navbar-brand mb-1 h1">{(usuarioLogueado != null) ? usuarioLogueado.nombre : ''}</span>
-          <Button onClick={Logout}>Logout</Button>
-        </div>
-      </nav>
-
-      <br></br>
-      {
-        contenido
-      }
-    </div>
-  );
+const AppWrapper = () => {
+<QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
 }
 
 export default App;
